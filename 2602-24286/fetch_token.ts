a0375bf7f8b133446fc8c67d1db1973abc08c60a
AI圈的9527 <wcsop-hermes>
2026-07-14 07:25:46 +0800

    auto: 2602-24286, 602-24286

--- 2602-24286/fetch_token.ts
+++
@@ -1,9 +0,0 @@
-import { loadWechatExtendConfig, resolveAccount, loadCredentials } from "./_wechat_extend_config.ts";
-import fs from "node:fs";
-const extConfig = loadWechatExtendConfig();
-const resolved = resolveAccount(extConfig, undefined);
-const creds = loadCredentials(resolved);
-const tokenUrl = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${creds.appId}&secret=${creds.appSecret}`;
-const token = (await (await fetch(tokenUrl)).json()).access_token;
-fs.writeFileSync("./_token.txt", token);
-console.log("token:", token.slice(0,8) + "...");
