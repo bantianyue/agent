86adb4ef4f960a47d39ccfb4077d1aae7cd75c15
AI圈的9527 <wcsop-hermes>
2026-09-23 14:03:59 +0800

    auto: evs-architecture-unmasked, jevs-architecture-unmasked (+1, ~1)

--- jevs-architecture-unmasked/_probe.py
+++ jevs-architecture-unmasked/_probe.py
@@ -1,22 +1,29 @@
-import urllib.request, re, ssl, json, sys
+import urllib.request, re, ssl, os, sys
+for k in ("HTTP_PROXY","HTTPS_PROXY","http_proxy","https_proxy","ALL_PROXY","all_proxy"):
+    os.environ.pop(k, None)
+os.environ["NO_PROXY"] = "*"
 ctx = ssl.create_default_context(); ctx.check_hostname=False; ctx.verify_mode=ssl.CERT_NONE
 url = "https://archerhume.com/posts/jevs-architecture-unmasked/?v=3"
+opener = urllib.request.build_opener(urllib.request.ProxyHandler({}))
 req = urllib.request.Request(url, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0 Safari/537.36"})
-h = urllib.request.urlopen(req, timeout=60, context=ctx).read().decode("utf-8", "replace")
+h = opener.open(req, timeout=90).read().decode("utf-8", "replace")
 open("_page.html","w",encoding="utf-8").write(h)
-print("len", len(h))
-print("img", len(re.findall(r'<img', h)))
-print("iframe", len(re.findall(r'<iframe', h)))
-print("svg", len(re.findall(r'<svg', h)))
-print("canvas", len(re.findall(r'<canvas', h)))
-print("figure", len(re.findall(r'<figure', h)))
-print("pre", len(re.findall(r'<pre', h)))
-print("script", len(re.findall(r'<script', h)))
-print("h2", len(re.findall(r'<h2', h)))
-print("h3", len(re.findall(r'<h3', h)))
-for m in re.findall(r'<img[^>]*>', h)[:40]:
-    print("IMG:", m[:220])
+out=[]
+out.append("len %d" % len(h))
+for tag in ("img","iframe","svg","canvas","figure","figcaption","pre","script","h2","h3","table","video"):
+    out.append("%s %d" % (tag, len(re.findall(r'<'+tag, h))))
+out.append("rAF %d paint( %d" % (h.count("requestAnimationFrame"), h.count("paint(")))
+out.append("--- IMG ---")
+for m in re.findall(r'<img[^>]*>', h)[:60]:
+    out.append(m[:260])
+out.append("--- IFRAME ---")
 for m in re.findall(r'<iframe[^>]*>', h)[:20]:
-    print("IFRAME:", m[:300])
-print("--- rAF/paint ---")
-print("rAF", h.count("requestAnimationFrame"), "paint(", h.count("paint("))
+    out.append(m[:300])
+out.append("--- SVG inline ---")
+for m in re.findall(r'<svg[^>]*>', h)[:20]:
+    out.append(m[:200])
+out.append("--- object ---")
+for m in re.findall(r'<object[^>]*>', h)[:20]:
+    out.append(m[:260])
+open("_probe.txt","w",encoding="utf-8").write("\n".join(out))
+print("done")
